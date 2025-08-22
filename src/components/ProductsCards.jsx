b90c462fe725dc1e50c30ea4132fd3100b1f41ba
kaanarslann <kaanarslan_hp@hotmail.com>
2025-08-22 13:21:55 +0300

    adds array control to bestsellers

--- src/components/ProductsCards.jsx
+++ src/components/ProductsCards.jsx
@@ -13,7 +13,7 @@ export default function ProductCards() {
     const categories = useSelector((state) => state.product.categories);
 
     useEffect(() => {
-        if(bestSellers.length === 0) {
+        if(Array.isArray(bestSellers) && bestSellers.length === 0) {
             dispatch(getBestSellers());
         }
     }, []);
