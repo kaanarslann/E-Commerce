8047c6d9765037c72853b043cfba6663ec1b42ce
kaanarslann <kaanarslan_hp@hotmail.com>
2025-08-22 18:11:14 +0300

    adds render deploy link to baseurl

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
         //baseURL: "https://workintech-fe-ecommerce.onrender.com",
-        baseURL: "http://localhost:3000/ecommerce",
+        //baseURL: "http://localhost:3000/ecommerce",
+        baseURL: "https://e-commerce-backend-mabd.onrender.com/ecommerce",
     });
 
 export default axiosInstance;
